105a0c3ef1c43570cc0c90280cca4a5536211bdc
Nam Nguyen <nam03031999@gmail.com>
2022-02-10 02:37:09 -0400

    init project

---
+++ Chat.Backend/src/main/java/com/namNguyen03/Chat/Backend/Application.java
@@ -0,0 +1,13 @@
+package com.namNguyen03.Chat.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
